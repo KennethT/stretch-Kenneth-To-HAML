1ed594852d0f0bfdc07f37be11871fb8a70575f7
Kenneth To <kennethkto@gmail.com>
2015-01-15 11:04:16 -0700

    added twitter bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,13 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+
+
+
 //= require jquery
 //= require jquery_ujs
+//= require jquery
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
